74885ca89554fb225f21423c73c9a7cbe6bbdbe4
Wills Liou <willsliou@protonmail.com>
2019-12-28 22:22:57 -0800

    main

--- past/index.js
+++ past/index.js
@@ -3,16 +3,16 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  // res.send('<h1>Hello world</h1>');
+  //res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
