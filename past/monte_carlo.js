d104436b58c007d0b2c4440f14e79d9266e19289
Wills Liou <willsliou@protonmail.com>
2020-02-24 10:03:03 -0800

    algo

--- past/monte_carlo.js
+++ past/monte_carlo.js
@@ -1,6 +1,7 @@
 // Why we are using MCTS
-// Minimax Algorithm's main weaness is needing to expand through the entire game tree. (weak to high branching factor games);
-//
+// Minimax Algorithm's main weakness is needing to expand through the entire game tree. (weak to high branching factor games);
+// Alpha-beta pruning is another alternative; it does less searches than minimax, but essential the same
+// https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-034-artificial-intelligence-fall-2010/lecture-videos/lecture-6-search-games-minimax-and-alpha-beta/
 class MonteCarlo {
 
     runSearch(state, timeout) {
