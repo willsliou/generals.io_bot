0bb162bcbc4ffed44081ab874b975493f17a1873
Wills Liou <willsliou@protonmail.com>
2020-02-24 09:17:29 -0800

    tex

---
+++ past/monte_carlo.js
@@ -0,0 +1,14 @@
+// Why we are using MCTS
+// Minimax Algorithm's main weaness is needing to expand through the entire game tree. (weak to high branching factor games);
+//
+class MonteCarlo {
+
+    runSearch(state, timeout) {
+
+    }
+
+    bestPlay(state) {
+
+
+    }
+}
